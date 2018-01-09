f7cfc2ec21e53d83f4fdab8e45a875389d9aa429
Rachid <naaim.rachid@gmail.com>
2018-01-09 23:33:15 +0000

    Create main components of the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ShopComponent } from './shop/shop.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import {LoginService} from './login.service';
+import  {ShopService} from './shop.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent,
+    UserComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [LoginService, ShopService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
