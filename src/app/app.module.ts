9fc262d9ab0ddaf01e6fd4dd70b8e4173c2e9533
Rachid <naaim.rachid@gmail.com>
2018-01-10 00:21:53 +0000

    Set main and basic routes of the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes,RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ShopComponent } from './shop/shop.component';
@@ -8,6 +8,16 @@ import { UserComponent } from './user/user.component';
 import { LoginComponent } from './login/login.component';
 import {LoginService} from './login.service';
 import  {ShopService} from './shop.service';
+
+const myRoutes : Routes = [{
+  path : 'login',component : LoginComponent
+},{
+  path : 'shops',component : ShopComponent
+},{
+  path : 'shop/:username', component : UserComponent
+},{
+  path : '', redirectTo : '/', pathMatch : 'full'
+},];
 
 
 @NgModule({
@@ -18,7 +28,8 @@ import  {ShopService} from './shop.service';
     LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [LoginService, ShopService],
   bootstrap: [AppComponent]
